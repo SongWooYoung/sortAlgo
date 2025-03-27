f3119c329723b48062bd2bb517e70a8a975dd38a
songwooyoung <swy0121@unist.ac.kr>
2025-03-28 00:14:32 +0900

    selectionSort: complete

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -15,12 +16,19 @@ void selectionSort(vector<int>& array) {
     // Time Complexity : O(n^2)
     // find the minimun element in the unsorted part
     // swap the minimum element with the first element in the unsorted part
-
-    
-
+    size_t len = array.size();
+    for (size_t i = 0 ; i < len-1; i++) { // lotaiton number;
+        int min = INT_MAX;
+        size_t minIndex;
+        for (size_t j = i; j < len; j++) {
+            if (array.at(j) < min) {
+                min = array.at(j);
+                minIndex = j;
+            }
+        }
+        swap(array.at(i), array.at(minIndex));
+    }
 }
-
-
 
 
 int main() {
