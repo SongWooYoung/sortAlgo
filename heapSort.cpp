372a8cfa95e922d5352be511756408f1818d387a
songwooyoung <swy0121@unist.ac.kr>
2025-03-27 21:17:27 +0900

    amended logic of heapSort

--- heapSort.cpp
+++ heapSort.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
+#include <vector>
+
+#define stl 1<<31
 
 using namespace std;
 
+void heapSort(vector<int>& array) {
 // Heap Sort : make array into a heap and then sort it.
 // Time Complexity : O(nlogn)
-// 1. put all things into an array
-// 2. build max heap
-// 3. swap the first and last element
-// 4. heapify the array
-// 5. repeat 3 and 4 until the array is sorted
+// 1. build max heap => asceding order, min heap => descending order
+//    why? because when we swaps the last element with the first element, we want the largest element to be at the last index
+//    then, what is max heap? => the parent node is always larger than the child node
+    maxHeapify();
+
+    for (int i = array.size(); i > 0; i--) {
+
+        // 2. swap the first and last element // fix the max element to the last index
+        swap(array[1], array[i - 1]);
+        
+        // 3. heapify the array
+        heapify(); // root node has next largest element
+    }
+// 4. repeat 3 and 4 until the array is sorted
+
+}
 
 
 
 
 int main() {
 
-    int array[10] = {1, -1, 9, 6, 8, 7, -4, 50, 8, 10};
+    vector<int> array = {stl, 1, -1, 9, 6, 8, 7, -4, 50, 8, 10};
 
-    heapSort(array, 5);
+    heapSort(array);
 
     for (int i = 0; i < 10; i++) {
         cout << array[i] << " ";
