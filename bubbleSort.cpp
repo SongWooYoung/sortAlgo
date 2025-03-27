e697fb1052b4e485158544c3a875c529c201289d
songwooyoung <swy0121@unist.ac.kr>
2025-03-27 23:52:46 +0900

    bubbleSort: improve performance of BS

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -18,20 +18,17 @@ void bubbleSort(vector<int>& array) { // ascending order
     // at second completion, the second largest element goes to the second last index
     // in the end, the smallest element goes to the first index
     size_t len = array.size();
-    //cout << array << endl;
-    for (size_t j = 0; j < len; j++) {
-        for (size_t i = 0; i < len-1; i++) {
+    for (size_t j = 0; j < len-1; j++) { // last iteration is useless because there is only one that is unsorted
+        for (size_t i = 0; i < len-1-j; i++) { // every iteration, there is one element that is sorted
             if (array[i] > array[i+1]) {
                 swap(array[i], array[i+1]);
             }
         }
     }
-    //cout << array << endl;
+
+    // if possible, we can reduce the number of iterations
+    // because there are elemets already sorted
 }
-
-
-
-
 
 int main() {
 
