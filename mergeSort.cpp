cbc153498ae7b08310db2fb381773bf5c592cb9a
songwooyoung <swy0121@unist.ac.kr>
2025-03-27 15:34:01 +0900

    mergeSort 3

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -11,13 +11,36 @@ using namespace std;
 
 void merge(int* array, int left, int mid, int right){ 
     //the idea of merge
+    // 0. we need of course a variable that indicates the index of the array that we are going to put the value
     // 1. we know the left, mid, and right index of the array
     // 2. we declare indexL and indexR which are the indices that have lowest value of each array
+    int wheretoPut = left;
+    int indexL = left;
+    int indexR = mid + 1;
     // 3. we compare the value of the indexLV and indexRV and put the smaller value into the array
     // 4. we increase the indexL or indexR by 1
-    // 5. we need of course a variable that indicates the index of the array that we are going to put the value
+    while (indexL <= mid && indexR <= right) {
+        if (array[indexL] < array[indexR]) {
+            array[wheretoPut] = array[indexL];
+            indexL++;
+        } else {
+            array[wheretoPut] = array[indexR];
+            indexR++;
+        }
+        wheretoPut++;
+    }
+    
+    while (indexL <= mid) {
+        array[wheretoPut] = array[indexL];
+        indexL++;
+        wheretoPut++;
+    }
+    while (indexR <= right) {
+        array[wheretoPut] = array[indexR];
+        indexR++;
+        wheretoPut++;
+    }
 }
-
 
 
 void mergeSort(int* array, int left, int right) { // I want it to be in-place sorting
@@ -38,6 +61,10 @@ int main() {
     int array[10] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
 
     mergeSort(array, 0, 9);
+
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
 
     return 0;
 }
