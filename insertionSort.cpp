42b1a5c05ac587f8abb5575d10f75ba8700aaa29
songwooyoung <swy0121@unist.ac.kr>
2025-03-28 21:07:55 +0900

    insertionSort: Naive

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -7,17 +7,40 @@ using namespace std;
 // However, the limitation of array requires me to use heap memory
 // => I will use vector instead of array
 
+void insertionSort(vector<int>& array, size_t len) { // call by reference
+    // Insertion Sort : sort the array by inserting the element into the sorted part of the array
+    // Time Complexity : O(n^2)
+    // Do not use online sort the array
+    // Make temparray to store the sorted part of the array
+    // move the elements to the temparray
+    // then, insert the element into the temparray
+    // finally, copy the temparray to the original array
+    vector<int> tempArray;
 
-void insertionSort(vector<int>& array, int num) { // call by reference
+    for (size_t i = 0; i < len; i++) {
+        bool inserted = false;
 
-    int len = array.size();
-    for (int i = 0; i < len; i++) {
-        if (num < array[i]) {
-            array.insert(array.begin() + i, num); // insert have to use iteration
-            return;
+        // if tempArray is empty, push
+        if (tempArray.empty()) {
+            tempArray.push_back(array[i]);
+            continue;
+        }
+
+        for (size_t j = 0; j < tempArray.size(); j++) {
+            if (array[i] < tempArray[j]) {
+                tempArray.insert(tempArray.begin() + j, array[i]);
+                inserted = true;
+                break;
+            }
+        }
+
+        if (!inserted) {
+            // if the element is larger than all elements in tempArray
+            tempArray.push_back(array[i]);
         }
     }
-    array.push_back(num);
-
-    // previous code unnecessarily used if statement
+    for (size_t i = 0; i < len; i++) {
+        array[i] = tempArray[i];
+    }
 }
+
