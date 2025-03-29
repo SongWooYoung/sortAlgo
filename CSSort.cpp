496ae587b3c80f3d747fae8aa78bc988330aa35a
songwooyoung <swy0121@unist.ac.kr>
2025-03-30 02:06:47 +0900

    CSSort: if there is no swap, break

--- CSSort.cpp
+++ CSSort.cpp
@@ -6,27 +6,34 @@ using namespace std;
 
 
 void CSSort(vector<int>& array) {
-    bool LeftorRight = true; //true -> left false -> right
+    bool LeftorRight = true; //true -> left / false -> right
     size_t len = array.size();
     size_t Llimit = 0;
     size_t Rlimit = len-1;
     while(Llimit < Rlimit) {
+        // swap이 발생하지 않으면 while 탈출 필요요
+        bool occurSwap = false;
         if (LeftorRight == true) {
             for(size_t i = Llimit; i < Rlimit; i++) {
-                if (array.at(i) > array.at(i+1))
+                if (array.at(i) > array.at(i+1)){
                     swap(array.at(i), array.at(i+1));
+                    occurSwap = true;
+                }
             }
             Rlimit--;
             LeftorRight = false;
         }
         else {
             for(size_t i = Rlimit; i > Llimit; i--) {
-                if (array.at(i) < array.at(i-1))
+                if (array.at(i) < array.at(i-1)){
                     swap(array.at(i), array.at(i-1));
+                    occurSwap = true;
+                }
             }
             Llimit++;
             LeftorRight = true;
         }
+        if (occurSwap == false) break;
     }
 }
 
