b02c93a5278968037dd7c571944f28face566895
songwooyoung <swy0121@unist.ac.kr>
2025-03-30 02:18:51 +0900

    CSSort: complete

--- CSSort.cpp
+++ CSSort.cpp
@@ -10,9 +10,15 @@ void CSSort(vector<int>& array) {
     size_t len = array.size();
     size_t Llimit = 0;
     size_t Rlimit = len-1;
+
+    if(array.at(Llimit) >= array.at((Rlimit + Rlimit)/2) && array.at((Rlimit + Rlimit)/2) >= array.at(Rlimit))
+        LeftorRight = false;
+    //내림차순 해결해 보려고 넣어지만 다른 예제를 10퍼 더 빨리 해결할뿐 24번째 줄이 오름차순을 정하고 있어 불가능...
+
     while(Llimit < Rlimit) {
-        // swap이 발생하지 않으면 while 탈출 필요요
+        // swap이 발생하지 않으면 while 탈출 필요
         bool occurSwap = false;
+
         if (LeftorRight == true) {
             for(size_t i = Llimit; i < Rlimit; i++) {
                 if (array.at(i) > array.at(i+1)){
@@ -23,6 +29,7 @@ void CSSort(vector<int>& array) {
             Rlimit--;
             LeftorRight = false;
         }
+
         else {
             for(size_t i = Rlimit; i > Llimit; i--) {
                 if (array.at(i) < array.at(i-1)){
@@ -33,6 +40,7 @@ void CSSort(vector<int>& array) {
             Llimit++;
             LeftorRight = true;
         }
+
         if (occurSwap == false) break;
     }
 }
