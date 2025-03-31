4a84f2f8aa6b406a56d076a3aaae9aa0a3deaeb2
songwooyoung <swy0121@unist.ac.kr>
2025-03-31 21:40:27 +0900

    TSort and libSort completion

--- TSort.cpp
+++ TSort.cpp
@@ -5,61 +5,86 @@
 
 using namespace std;
 
-struct Node {
-    int key;
-    vector<int> history;
-    int originalIndex;
-};
-
-int compare(int a, int b, vector<Node>& array) {
-    if (array[a].key <= array[b].key) {
-        array[a].history.push_back(b);
-        return a;
-    } else {
-        array[b].history.push_back(a);
-        return b;
+int up(vector<int>& array, int left, int right) { // 0 1 2-3  4-7   8-15 최초에만 사용
+    if (left == 1) return 1;
+    for (int i = left; i <= right; i++) {
+        if (array.at(i/2) > array.at(i)) array.at(i/2) = array.at(i); 
     }
+    int n = up(array, left/2, left-1);
+    if (array.at(2 * n) == array.at(n))     return 2 * n;
+    else                                    return 2 * n + 1;
 }
 
-int tournamentPhase(vector<Node>& array) {
-    vector<int> current;
-    for (size_t i = 0; i < array.size(); ++i) current.push_back(i);
+int update(vector<int>& array, int minIdx) {
+    int idx = minIdx;
+    array.at(idx) = INT_MAX;
 
-    while (current.size() > 1) {
-        vector<int> next;
-        for (size_t i = 0; i + 1 < current.size(); i += 2) {
-            int winner = compare(current[i], current[i + 1], array);
-            next.push_back(winner);
-        }
-        if (current.size() % 2 == 1) {
-            next.push_back(current.back());
-        }
-        current = next;
+    // 부모 노드 업데이트
+    while (idx > 1) {
+        int sibling = (idx % 2 == 0) ? idx + 1 : idx - 1;
+        int parent = idx / 2;
+
+        if (sibling < array.size() && array.at(idx) < array.at(sibling))
+            array.at(parent) = array.at(idx);
+        else if (sibling < array.size())
+            array.at(parent) = array.at(sibling);
+        else
+            array.at(parent) = array.at(idx);  // sibling이 없을 수도 있음
+
+        idx = parent;
     }
 
-    return current[0];
+    // 루트부터 내려가서 현재 최소값 인덱스 찾기
+    int n = 1;
+    while (2 * n < array.size()) {
+        if (array.at(2 * n) <= array.at(2 * n + 1))
+            n = 2 * n;
+        else
+            n = 2 * n + 1;
+    }
+    return n;
 }
 
-vector<int> TSort(vector<Node> array) {
-    vector<int> arr;
-    return arr;
+
+vector<int> TSort(vector<int>& data) {
+    // list를 확장 -> 2^k개 
+    int originalLen = data.size(); // 원래 길이 저장
+    int len = data.size();
+    int padNum = 1;
+    while(padNum < len) padNum *= 2;
+    for (int i = len; i < padNum; i++) data.push_back(INT_MAX);
+    len = data.size();
+
+    //
+    vector<int> newList(2 * len);
+    for (int i = 0; i < len; i++) newList.at(i) = INT_MAX; // 
+    for (int i = 0; i < len; i++) newList.at(len + i) = data.at(i); // 
+
+    // up -> 각 홀수번째와 짝수번째를 비교해서 더 작은 값 올기
+    // update () -> 최소값을 찾고 변경했으면 해당 인덱스만 변경 후 비교교
+    // root에 도달한값 result vector에 push_back => 해당 자리 INT_MAX로 채우고 다시 up진행
+    vector<int> result;
+    int minIdx = up(newList, (int) len, (int) len*2-1);
+    result.push_back(newList.at(minIdx));
+
+    for (int i = 0 ; i < len-1; i++) {
+        int n = update(newList, minIdx);
+        result.push_back(newList.at(1)); 
+        minIdx = n;
+    }
+
+    while (result.size() > originalLen) result.pop_back();
+    return result;
 }
 
 
-// int main() {
-//     vector<int> data = {10, 4, 3, 7, 9, 6, 5, 1};
-//     vector<Node> array;
-//     for (int val : data) {
-//         array.push_back({val, {}, 0});
-//     }
+int main() {
+    vector<int> data = {10, 4, 3, 7, 9, 6, 5, 1, 100, 83, 27, 2, 11};
+    vector<int> result = TSort(data);
 
-//     vector<int> result = TSort(array);
+    for (int i = 0 ; i < result.size(); i++) {
+        cout << result.at(i) << endl;
+    }
 
-//     cout << "Sorted Result: ";
-//     for (int val : result) {
-//         cout << val << " ";
-//     }
-//     cout << endl;
-
-//     return 0;
-// }
+    return 0;
+}
--- librarySort.cpp
+++ librarySort.cpp
@@ -8,15 +8,12 @@ size_t binarySearch(const vector<pair<int, char>>& array, size_t *lastIndex, int
 
     while (left < right) {
         size_t mid = (left + right) / 2;
-
-        // Skip 'X' by checking nearest valid 'O'
         size_t midValid = mid;
         bool found = false;
-
         if (array[mid].second == 'X') {
             // Search left
             size_t l = mid;
-            while (l > left) {
+            while (l > left && l < array.size()) {
                 l--;
                 if (array[l].second == 'O') {
                     midValid = l;
@@ -24,8 +21,6 @@ size_t binarySearch(const vector<pair<int, char>>& array, size_t *lastIndex, int
                     break;
                 }
             }
-
-            // Search right if left failed
             if (!found) {
                 size_t r = mid;
                 while (r + 1 < right) {
@@ -37,15 +32,8 @@ size_t binarySearch(const vector<pair<int, char>>& array, size_t *lastIndex, int
                     }
                 }
             }
-
-            if (!found) {
-                // 전체가 빈칸이면 그냥 삽입 지점 반환
-                return left;
-            }
-        } else {
-            midValid = mid;
-        }
-
+            if (!found) return left;
+        } else midValid = mid;
         if (array[midValid].first < input)
             left = midValid + 1;
         else
@@ -54,42 +42,58 @@ size_t binarySearch(const vector<pair<int, char>>& array, size_t *lastIndex, int
     return left;
 }
 
+size_t searchingPlace(vector<pair<int,char>>& array, size_t* lastIndex, bool right, size_t currentIdx) {
+    int input = array[*lastIndex].first;
 
-void insert(vector<pair<int, char>>& array, size_t num, size_t* lastIndex) { // array(정렬된 부분분) 크기 안변함
-    // array는 현재 rebalancing이 된 상태라 가정
-    // 근데 만약 들어오는 개수가 1개면 이미 정렬되어 있다 가정 => 위에서 처리리
+    if (right) {
+        int wheretoPut = currentIdx + 1;
+        while (true) {
+            if (wheretoPut > (int)*lastIndex - 1) {
+                return searchingPlace(array, lastIndex, false, currentIdx);
+            }
+            if (array[wheretoPut].second == 'X') break;
+            wheretoPut++;
+        }
+        // Shift right
+        for (int i = wheretoPut; i > currentIdx; i--) {
+            array[i] = array[i - 1];
+        }
 
+        return currentIdx;
+        
+    } else {
+        int wheretoPut = currentIdx - 1;
+        while (true) {
+            if (wheretoPut < 0) {
+                return searchingPlace(array, lastIndex, true, currentIdx);
+            }
+            if (array[wheretoPut].second == 'X') break;
+            wheretoPut--;
+        }
+        // Shift left
+        for (int i = wheretoPut; i < currentIdx-1; i++) {
+            array[i] = array[i + 1];
+        }
+        array.at(wheretoPut).second = 'O';
+
+        return currentIdx - 1;
+    } 
+}
+
+void insert(vector<pair<int, char>>& array, size_t num, size_t* lastIndex) {
     size_t n = num;
     while(n > 0) {
-
         int input = array.at(*lastIndex).first;
-        //cout << "FUCK YOU" << endl;
         size_t currentIdx = binarySearch(array, lastIndex, input);
 
-        if (array.at(currentIdx).second == 'X') {
-            array.at(currentIdx) = {input, 'O'};
+        if (array.at(currentIdx).second == 'O') {
+            int idxToInsert = searchingPlace(array, lastIndex, true, currentIdx);
+            array[idxToInsert] = {input, 'O'};
+        } else {
+            array[currentIdx] = {input, 'O'};
         }
-        else {
-            size_t wheretoPut = currentIdx+1;
-            while(true) {
-                if (wheretoPut >= *lastIndex - 1) {
-                    array.insert(array.begin() + wheretoPut, make_pair(0, 'X'));
-                    *lastIndex = *lastIndex + 1;
-                    break;
-                }
-                if (array.at(wheretoPut).second == 'X') break;
-                wheretoPut++;
-            } 
-            array.at(wheretoPut).second = 'O';
-            rotate(array.begin() + currentIdx, array.begin() + wheretoPut, array.begin() + wheretoPut + 1);
-            array.at(currentIdx).first = input;
-        }
-        array.erase(array.begin() + (int)(*lastIndex));
 
-        // for (size_t i = 0; i < *lastIndex; i++) {
-        //     cout <<"(" <<array.at(i).first<< ", " << array.at(i).second << ")";
-        // }
-        // cout << endl;
+        array.erase(array.begin() + (int)(*lastIndex));
         n--;
     }
 }
@@ -102,8 +106,6 @@ void rebalancing(vector<pair<int, char>>& array, size_t* lastIndex) {
         *lastIndex += 2;
         return;
     }
-    // lastIndex부터 빈칸을 lastindex+1개 만큼 만들어줌
-    // lastIndex-1의 원소부터 내려가면서 second가 'O'면 index 두배에 집어넣기
     for (size_t i = 0; i < *lastIndex+1; i++) {
         array.insert(array.begin()+*lastIndex, make_pair<int, char>(0,'X'));
     }
@@ -133,7 +135,6 @@ void librarySort(vector<pair<int, char>>& array) {
     size_t lastIndex = 0; // 정렬된 부분의 array 크기 추적
     while (true) {
         if (lastIndex == 0) {
-            //cout << "first" << endl;
             lastIndex++;
             rebalancing(array, &lastIndex);
             NumbersofNextInput  = NumbersofNextInput << 1;
@@ -141,18 +142,12 @@ void librarySort(vector<pair<int, char>>& array) {
         }
         size_t remainingInputs = array.size() - lastIndex;
         if (NumbersofNextInput > remainingInputs) {
-            //cout << "LAST" << endl;
             insert(array, remainingInputs, &lastIndex);
             break;
         }
-        //cout << "WeAre PLAYING" << endl;
         insert(array, NumbersofNextInput, &lastIndex);
-        //cout << "After insertion" << endl;
         rebalancing(array, &lastIndex);
-        //cout << "After rebal" << endl;
         NumbersofNextInput  = NumbersofNextInput << 1;
-        //cout << "After increaseNNI" << endl;
-
     }
     deleteBlank(array);
 }
@@ -196,16 +191,7 @@ vector<int> generateRandomNumbers(int count, int minVal = 0, int maxVal = 9999) 
 //     cout << "=================================================================================================" << endl;
 
 //     vector<int> data = {
-//         100, 199, 298, 397, 496, 595, 694, 793, 892, 991,
-//         1090, 1189, 1288, 1387, 1486, 1585, 1684, 1783, 1882, 1981,
-//         2080, 2179, 2278, 2377, 2476, 2575, 2674, 2773, 2872, 2971,
-//         3070, 3169, 3268, 3367, 3466, 3565, 3664, 3763, 3862, 3961,
-//         4060, 4159, 4258, 4357, 4456, 4555, 4654, 4753, 4852, 4951,
-//         5050, 5149, 5248, 5347, 5446, 5545, 5644, 5743, 5842, 5941,
-//         6040, 6139, 6238, 6337, 6436, 6535, 6634, 6733, 6832, 6931,
-//         7030, 7129, 7228, 7327, 7426, 7525, 7624, 7723, 7822, 7921,
-//         8020, 8119, 8218, 8317, 8416, 8515, 8614, 8713, 8812, 8911,
-//         9010, 9109, 9208, 9307, 9406, 9505, 9604, 9703, 9802, 9901,
+//         100, 199, 1909 , 496, 2, 3,4,5,6,6,7,9, 595, 694, 298, 793, 892, 991, 397
 //     };
 //     vector<pair<int, char>> arr2;
 //     for (size_t i = 0; i < data.size(); i++) {
